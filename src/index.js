a735689ef96bac9fd4b487e473ad733202ee9019
Juhee Kang <56218979+developerjulie93@users.noreply.github.com>
2020-02-13 00:01:58 -0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-import Router from 'koa-router';
-import * as authCtrl from './auth.ctrl';
-
-const auth = new Router();
-auth.post('/register', authCtrl.register);
-auth.post('/login', authCtrl.login);
-auth.get('check', authCtrl.check);
-auth.post('/logout', authCtrl.logout);
-
-export default auth;
+require = require('esm')(module /*, options */);
+module.exports = require('./main.js');
