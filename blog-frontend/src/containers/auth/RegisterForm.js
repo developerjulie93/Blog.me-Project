6dbd0118f556f482dbf2d638293751eb75f61c34
Juhee Kang <56218979+developerjulie93@users.noreply.github.com>
2020-02-18 18:05:44 -0800

    Add files via upload

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,12 +1,14 @@
 import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, register} from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 
 const RegisterForm=()=>{
     const dispatch = useDispatch();
-    const {form} = useSelector(({auth}) => ({
-        form: auth.register
+    const {form, auth, authError} = useSelector(({auth}) => ({
+        form: auth.register,
+        auth: auth.auth,
+        authError: auth.authError
     }));
     const onChange = e =>{
         const {value, name} = e.target;
@@ -20,11 +22,28 @@ const RegisterForm=()=>{
     };
     const onSubmit = e =>{
         e.preventDefault();
+        const {username, password, passwordConfirm} = form;
+        if(password !== passwordConfirm){
+            return;
+        }
+        dispatch(register({username, password})); 
     };
+    //form initialize
     useEffect(()=>{
         dispatch(initializeForm('register'));
     }, [dispatch]);
-    
+    //register success/failure
+    useEffect(()=>{
+        if(authError){
+            console.log("Register Error");
+            console.log(authError);
+            return;
+        }
+        if(auth){
+            console.log("Register Success");
+            console.log(auth);
+        }
+    }, [auth, authError]);
     return(
         <AuthForm
             type="register"
