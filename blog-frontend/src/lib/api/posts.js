9dab7ed444cd9bc2a268bbcd7047a96362136b0b
Juhee Kang <56218979+developerjulie93@users.noreply.github.com>
2020-03-10 23:11:34 -0700

    Add files via upload

--- blog-frontend/src/lib/api/posts.js
+++ blog-frontend/src/lib/api/posts.js
@@ -1,17 +1,25 @@
-import client from './client';
 import qs from 'qs';
+import client from './client';
+
 
 export const writePost = ({title, body, tags}) => 
     client.post('/api/posts', {title, body, tags});
 
 export const readPost = id => client.get(`/api/posts/${id}`);
 
-export const listPosts = ({username, page, tag}) => {
+export const listPosts = ({page, username, tag}) => {
     const queryString = qs.stringify({
-        username,
         page,
+        username,
         tag,
     });
     
     return client.get(`/api/posts?${queryString}`);
 };
+export const updatePost = ({id, title, body, tags}) => 
+    client.patch(`/api/posts/${id}`, {
+        title,
+        body,
+        tags,
+    });
+    
